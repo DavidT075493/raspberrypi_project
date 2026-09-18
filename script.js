3047443725436e492e71669b025eaa02052d0990
jlealher990 <jlealher@depaul.edu>
2026-09-18 03:35:29 -0500

    Add button click event to show image

---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.getElementById("myButton");
+let image = document.getElementById("imageContainer");
+
+button.addEventListener("click", function() {
+    image.style.display = "block";
+});
